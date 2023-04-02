ced2ff911e326796001bbed05c76c62c41a7dba2
linchenxi <linchenxi@cloudkaifa.com>
2023-04-02 14:45:50 +0800

    Site updated: 2023-04-02 14:45:48

--- js/footer.js
+++ js/footer.js
@@ -9,7 +9,7 @@ $(document).ready(function(e){
 
 //本站运行时间
 function show_date_time(){
-$('.framework-info').html('本站居然运行<span id="span_dt_dt" style="color: #fff;"></span>');
+$('.framework-info').html('本站居然运行了<span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("3/30/2023 0:0:0");
 today=new Date();
