402d6857ffa49d38accee84c9fc14a00f5132da7
linchenxi <linchenxi@cloudkaifa.com>
2023-04-02 12:48:28 +0800

    Site updated: 2023-04-02 12:48:27

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+
+<script>
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234", 
+    "background": "1",
+    "tmpColor": "FFFFFF", 
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "71251781e33f4fd89bae77ace374ab54"
+     }
+}
+</script>
+{/* <script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script> */}
