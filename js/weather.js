08dd9e0c3f8ed24225a59d39742303e3045ca069
linchenxi <linchenxi@cloudkaifa.com>
2023-04-02 13:15:09 +0800

    Site updated: 2023-04-02 13:15:07

--- js/weather.js
+++ js/weather.js
@@ -13,10 +13,9 @@ WIDGET = {
     "padding": "10px 10px 10px 10px",
     "shadow": "0",
     "language": "auto",
-    "fixed": "true",
+    "fixed": "false",
     "vertical": "top",
     "horizontal": "left",
-    "city": "CN101010100",
     "key": "71251781e33f4fd89bae77ace374ab54"
      }
 }
